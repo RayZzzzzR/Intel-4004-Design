7afde73bd1c741f423b045edd8e4ade39abe3e47
Somya Kumar <164711749+RayZzzzzR@users.noreply.github.com>
2025-07-25 23:13:31 +0530

    Update instruction_pointer.v

--- instruction_pointer.v
+++ instruction_pointer.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
 `default_nettype none
-////////////////////////////////////////////////////////////////////////
-// 
-// 4004 Instruction Pointer Array
-// 
-// This file is part of the MCS-4 project hosted at OpenCores:
-//      http://www.opencores.org/cores/mcs-4/
-// 
-// Copyright © 2012 by Reece Pollack <rrpollack@opencores.org>
-// 
-// These materials are provided under the Creative Commons
-// "Attribution-NonCommercial-ShareAlike" Public License. They
-// are NOT "public domain" and are protected by copyright.
-// 
-// This work based on materials provided by Intel Corporation and
-// others under the same license. See the file doc/License for
-// details of this license.
-//
-////////////////////////////////////////////////////////////////////////
 
 module instruction_pointer (
 	input  wire			sysclk,					// 50 MHz FPGA clock
